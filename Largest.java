8c0b302047d7bce25c5fa1127aa7d730cb7f03b6
karan2108 <77577426+karan2108@users.noreply.github.com>
2025-01-12 22:22:42 +0530

    Updated

--- Largest.java
+++ Largest.java
@@ -1,10 +1,22 @@
 // Find Largest Among three numbers
 
+import java.util.Scanner;
+
 public class Largest {
 
     public static void main(String[] args) {
 
-        double n1 = -4.5, n2 = 3.9, n3 = 2.5;
+        // Create scanner object for user input 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.pirntln("Enter 1st number : ");
+        double n1 = scanner.nextDouble;
+
+        System.out.pirntln("Enter 2nd number : ");
+        double n2 = scanner.nextDouble;
+
+        System.out.pirntln("Enter 3rd number : ");
+        double n3 = scanner.nextDouble;
 
         if( n1 >= n2 && n1 >= n3)
             System.out.println(n1 + " is the largest number.");
